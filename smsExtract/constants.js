7b769b2bf1aaa5373a609ae804457f4159c5cbf4
Sam-2019 <samtmartey@gmail.com>
2022-01-15 19:57:48 +0000

    added constants

---
+++ smsExtract/constants.js
@@ -0,0 +1,4 @@
+export const identifier_vodafone = "00000";
+export const withdarawal_airteltigo = "You have withdrawn";
+export const customer_airteltigo = "Dear Customer";
+export const receipt_airteltigo = "You have received";
