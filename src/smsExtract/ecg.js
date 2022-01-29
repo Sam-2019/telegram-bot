a093d43e6c7f00e86137bdad9c058ced29ed5f9f
Sam-2019 <samtmartey@gmail.com>
2022-01-29 17:14:48 +0000

    updated ecg regex

--- src/smsExtract/ecg.js
+++ src/smsExtract/ecg.js
@@ -8,7 +8,8 @@ const year_pattern =
 //eslint-disable-next-line
 const account_number_pattern = /(ACC).?(#)\:.?([0-9]+\-[0-9]+\-[0-9]+)/gm;
 //eslint-disable-next-line
-const account_name_and_id_pattern = /([A-Z]+ [A-Z]+ [A-Z]+)\-([0-9]+)/gim;
+const account_name_and_id_pattern =
+  /([A-Z]+ [A-Z]+ ?[A-Z]+ ?[A-Z]+)\-([0-9]+)/gim;
 //eslint-disable-next-line
 const prev_usage_pattern = /PREV+\:.?([0-9]+)/gm;
 //eslint-disable-next-line
@@ -36,45 +37,35 @@ const balance_due_pattern = /BAL.?DUE+\:.?([0-9]+).?([0-9]+)/gm;
 //eslint-disable-next-line
 const due_by_pattern = /DUE.?BY\:.?(?<day>([0-9]+)\-([a-zA-Z]+)\-([0-9]+))/gm;
 
+const number_pattern = /\d+.?\d+.?\d+/g;
+const billable_period_pattern = /([A-Z]+)-\d+ /g;
+
 export const checkECG = (data) => {
-  console.log(data);
 
-  let month = data.match(month_pattern);
-  let year = data.match(year_pattern);
-  let account_number = data.match(account_number_pattern);
-  let account_name_and_id = data.match(account_name_and_id_pattern);
-  let prev_usage = data.match(prev_usage_pattern);
-  let new_usage = data.match(new_usage_pattern);
-  let usage_rate = data.match(usage_rate_pattern);
-  let usage_qty = data.match(usage_qty_pattern);
-  let usage = data.match(usage_pattern);
-  let water_charge = data.match(water_charge_pattern);
-  let fire_tax = data.match(fire_tax_pattern);
-  let rural_tax = data.match(rural_tax_pattern);
-  let service_charge = data.match(service_charge_pattern);
-  let month_total = data.match(month_total_pattern);
-  let prev_balance = data.match(prev_balance_pattern);
-  let balance_due = data.match(balance_due_pattern);
-  let due_by = data.match(due_by_pattern);
+  let billable_period = String(data.match(billable_period_pattern)).trim();
+  let digits = data.match(number_pattern);
+  let due_by = String(data.match(due_by_pattern)).substring(8) 
+  let account_name = String(data.match(account_name_and_id_pattern)).slice(0, -4);
+  let usage_qty = String(data.match(usage_qty_pattern)).substring(6)
 
-  console.log({ month: month });
-  console.log({ year: year });
-  console.log({ account_number: account_number });
-  console.log({ account_name_and_id: account_name_and_id });
+  console.log({ bill_period: billable_period });
+  console.log({ account_number: digits[1] });
+  console.log({ account_name: account_name });
+  console.log({ account_id: digits[2] });
 
-  console.log({ prev_usage: prev_usage });
-  console.log({ new_usage: new_usage });
-  console.log({ usage_rate: usage_rate });
+  console.log({ prev_usage: digits[3] });
+  console.log({ new_usage: digits[4] });
   console.log({ usage_qty: usage_qty });
+  console.log({ usage_rate: digits[5] });
 
-  console.log({ usage: usage });
-  console.log({ water_charge: water_charge });
-  console.log({ fire_tax: fire_tax });
-  console.log({ rural_tax: rural_tax });
+  console.log({ water_charge: digits[6] });
+  console.log({ fire_tax: digits[7] });
+  console.log({ rural_tax: digits[8] });
 
-  console.log({ service_charge: service_charge });
-  console.log({ month_total: month_total });
-  console.log({ prev_balance: prev_balance });
-  console.log({ balance_due: balance_due });
-  console.log({ due_by: due_by });
+  console.log({ service_charge: digits[9] });
+  console.log({ month_total: digits[10] });
+  console.log({ prev_balance: digits[11] });
+  console.log({ last_paid: digits[12] });
+  console.log({ balance_due: digits[13] });
+  console.log({ due_by: due_by});
 };
