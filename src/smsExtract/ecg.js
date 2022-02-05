b9044aa1657961a4ee27e9fea3ec7729233af710
Sam-2019 <samtmartey@gmail.com>
2022-02-05 19:24:49 +0000

    added ecg regex pattern

--- src/smsExtract/ecg.js
+++ src/smsExtract/ecg.js
@@ -1,7 +1,22 @@
+//eslint-disable-next-line
+const numbers_pattern = /:([0-9]+)/gm;
+//eslint-disable-next-line
+const name_pattern =
+  /Customer:([a-zA-Z]+) ([a-zA-Z]+) ([a-zA-Z]+) ([a-zA-Z]+)/gm;
+
 export const checkECG = (data) => {
-  // console.log({  });
+  let numbers = data.match(numbers_pattern);
+  let name = String(data.match(name_pattern)).substring(9);
+
+  // console.log({ meter_number: numbers[0].substring(1) });
+  // console.log({ credit: numbers[1].substring(1) });
+  // console.log({ name: name });
 
   return {
     Bill: data,
+
+    name: name ? name : null,
+    meter_number: numbers ? numbers[0].substring(1) : null,
+    credit: numbers ? numbers[1].substring(1) : null,
   };
 };
